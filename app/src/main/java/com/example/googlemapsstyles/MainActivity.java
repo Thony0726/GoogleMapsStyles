56b6cfa8b630b21f303da02989bafe73fd6f8a8f
Thony0726 <richardanto0726@gmail.com>
2022-03-17 18:57:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemapsstyles/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.googlemapsstyles;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Botton nav
+        BottomNavigationView btnNav = findViewById(R.id.bottomNavigationView);
+        btnNav.setOnNavigationItemSelectedListener(navListener);
+        //Iniciamos el fragmato que deseamos
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, new MapsFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.itemMapa:
+                    selectedFragment = new MapsFragment();
+                    Toast.makeText(MainActivity.this, "Mapa",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, selectedFragment).commit();
+            return true;
+        }
+    };
+    /*se controla la pulsacion del boton atras y cierra la aplicacion*/
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder myBulid = new AlertDialog.Builder(this);
+        myBulid.setMessage("¿Deseas salir de la aplicacion?");
+        myBulid.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        myBulid.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = myBulid.create();
+        dialog.show();
+    }
+}
