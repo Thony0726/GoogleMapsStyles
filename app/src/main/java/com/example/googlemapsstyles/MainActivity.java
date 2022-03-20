fa931646184960bc247d4fd0e33afb619d74bb68
Oscar45ReinosoQ <98630891+Oscar45ReinosoQ@users.noreply.github.com>
2022-03-20 14:38:23 -0500

    cambios

--- app/src/main/java/com/example/googlemapsstyles/MainActivity.java
+++ app/src/main/java/com/example/googlemapsstyles/MainActivity.java
@@ -10,24 +10,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+//    private Button btnMarcador;
+    private GoogleMap map;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Botton nav
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+//        btnMarcador = (Button) findViewById(R.id.btnMarcador);
+//
+//        btnMarcador.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                insertarMarcador();
+//            }
+//        });
+////
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+
+//        Botton nav
         BottomNavigationView btnNav = findViewById(R.id.bottomNavigationView);
         btnNav.setOnNavigationItemSelectedListener(navListener);
         //Iniciamos el fragmato que deseamos
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, new MapsFragment()).commit();
+
     }
+
+//    private void insertarMarcador() {
+//        mapa.addMarker(new MarkerOptions()
+//                .position(new LatLng(40.3936945, -3.701519))
+//                .title("Pais: España"));
+//    }
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -35,13 +77,14 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.itemMapa:
                     selectedFragment = new MapsFragment();
-                    Toast.makeText(MainActivity.this, "Mapa",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Mapa", Toast.LENGTH_SHORT).show();
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, selectedFragment).commit();
             return true;
         }
     };
+
     /*se controla la pulsacion del boton atras y cierra la aplicacion*/
     @Override
     public void onBackPressed() {
@@ -62,4 +105,9 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = myBulid.create();
         dialog.show();
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/googlemapsstyles/MapsFragment.java
+++ app/src/main/java/com/example/googlemapsstyles/MapsFragment.java
@@ -8,15 +8,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
+
+
+    private Button btnvista;
+    private GoogleMap map;
+
+    private final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+    private Marker markerPerth;
+    private Marker markerSydney;
+    private Marker markerBrisbane;
+
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
@@ -26,6 +42,7 @@ public class MapsFragment extends Fragment {
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     };
+
 
     @Nullable
     @Override
@@ -43,5 +60,55 @@ public class MapsFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+    }
+
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+            Toast.makeText(this, marker.getTitle() +
+                            " has been clicked " + clickCount + " times.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+         Return false to indicate that we have not consumed the event and that we wish
+         for the default behavior to occur (which is for the camera to move such that the
+         marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+
+
+
+
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        // Add some markers to the map, and add a data object to each marker.
+        markerPerth = map.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth"));
+        markerPerth.setTag(0);
+
+        markerSydney = map.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney"));
+        markerSydney.setTag(0);
+
+        markerBrisbane = map.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane"));
+        markerBrisbane.setTag(0);
+
+        // Set a listener for marker click.
+        map.setOnMarkerClickListener(this);
+
     }
 }
